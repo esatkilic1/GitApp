dc9c61d940a10508630e111215ba5263def63e5e
Toprak <esatkilic2022@outlook.com>
2025-01-03 22:53:45 +0300

    v1

---
+++ src/Runner.java
@@ -0,0 +1,16 @@
+public class Runner {
+    public static void main(String[] args) {
+        //version 1
+        System.out.println("degisiklik 1"); //working space
+
+        // verisiyon 2
+        System.out.println("degisiklik yaptim");
+
+
+
+
+
+
+
+    }
+}
